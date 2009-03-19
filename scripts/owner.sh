35db1d55e63667a3fa1200983a13f5489473d101
Dustin L. Howett <dustin@howett.net>
2009-03-19 00:10:23 +0000

    Add short-circuit searching to the owner script: check for a package named after the App's bundle name and title first. (this was supposed to be part of a previous commit.)

--- scripts/owner.sh
+++ scripts/owner.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 cd /var/lib/dpkg/info
+BUNDLE=$1
+TITLE=$2
+
+echo "$0 $*" >> /var/log/CYDELETE.log
+echo "Checking $BUNDLE.list and $TITLE.list" >> /var/log/CYDELETE.log
+if [[ -e "$BUNDLE.list" ]]; then
+	echo -n $BUNDLE
+	exit 0
+elif [[ -e "$TITLE.list" ]]; then
+	echo -n $TITLE
+	exit 0
+fi
+
+shift 2
 OUT=$(grep "^$*\$" *.list | cut -d':' -f1)
 if [[ -z "$OUT" ]]; then exit 1;
 else echo -n ${OUT%.list}; exit 0; fi
