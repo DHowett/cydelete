d89a2c97e22e687a9d86930fff288019acc50dc1
Dustin L. Howett <dustin@howett.net>
2009-03-29 06:08:40 +0000

    Change uninstall_dpkg.sh to use apt-get, with a major hack for emulating the cydia pipe (use stderr, filter stderr for the "finish" line, and then cut from there.)

--- scripts/uninstall_dpkg.sh
+++ scripts/uninstall_dpkg.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 export PATH=/usr/bin:/bin:/sbin:/usr/sbin
-rm /tmp/cydiapipe &> /dev/null
-exec 3<>/tmp/cydiapipe
-export CYDIA="3 1"
-/usr/bin/dpkg -r $1 &>/dev/null
+export CYDIA="2 1"
+apt-get -y remove $1 2>&1 1>/dev/null | grep finish > /tmp/cydiapipe
 exitcode=$?
 echo -n $(cat /tmp/cydiapipe | cut -d':' -f2)
-rm /tmp/cydiapipe &> /dev/null
+rm -rf /tmp/cydiapipe &> /dev/null
 exit $exitcode
