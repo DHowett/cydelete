a6e29a338c1e52d346e4ad6c247624be56254a2c
Dustin L. Howett <dustin@howett.net>
2009-03-29 23:27:00 +0000

    Fix the exit code; I was checking grep's exit code, not apt's. Uses another tempfile.

--- scripts/uninstall_dpkg.sh
+++ scripts/uninstall_dpkg.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 export PATH=/usr/bin:/bin:/sbin:/usr/sbin
 export CYDIA="2 1"
-apt-get -y remove $1 2>&1 1>/dev/null | grep finish > /tmp/cydiapipe
+apt-get -y remove $1 2>/tmp/aptoutput 1>/dev/null
 exitcode=$?
+grep finish /tmp/aptoutput > /tmp/cydiapipe
+rm -rf /tmp/aptoutput &> /dev/null
 echo -n $(cat /tmp/cydiapipe | cut -d':' -f2)
 rm -rf /tmp/cydiapipe &> /dev/null
 exit $exitcode
