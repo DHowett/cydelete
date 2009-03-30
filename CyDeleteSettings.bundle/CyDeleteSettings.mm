db4cdec65b79aafb72ee7b5ec014fd6e6acdfa5c
Dustin L. Howett <dustin@howett.net>
2009-03-30 14:05:13 +0000

    Add a donation button to the preferences bundle.

--- CyDeleteSettings.bundle/CyDeleteSettings.mm
+++ CyDeleteSettings.bundle/CyDeleteSettings.mm
@@ -6,4 +6,8 @@
 	return [self localizedSpecifiersForSpecifiers:[self loadSpecifiersFromPlistName:@"CyDelete" target:self]];
 }
 
+- (id)donationButton:(id)arg {
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4275311"]];
+}
+
 @end
