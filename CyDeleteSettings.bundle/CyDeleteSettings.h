4b2ad869e8f97909fa84b46d2aafb78c272ecbc4
Dustin L. Howett <dustin@howett.net>
2009-04-01 18:12:17 +0000

    Add some checking (currently unused) for Cydia and Icy when we init the pref bundle.

--- CyDeleteSettings.bundle/CyDeleteSettings.h
+++ CyDeleteSettings.bundle/CyDeleteSettings.h
@@ -1,6 +1,8 @@
 #import "LocalizedListController.h"
 
 @interface CyDeleteSettingsController : LocalizedListController {
+	bool _cydiaPresent;
+	bool _icyPresent;
 }
 - (id)specifiers;
 @end
--- CyDeleteSettings.bundle/CyDeleteSettings.mm
+++ CyDeleteSettings.bundle/CyDeleteSettings.mm
@@ -2,6 +2,14 @@
 
 @implementation CyDeleteSettingsController
 
+- (void)viewDidBecomeVisible {
+	NSFileManager *manager = [NSFileManager defaultManager];
+	_cydiaPresent = [manager fileExistsAtPath:@"/Applications/Cydia.app/Info.plist"];
+	_icyPresent = [manager fileExistsAtPath:@"/Applications/Icy.app/Info.plist"];
+//	if(!_cydiaPresent) [self removeSpecifier:[_specifiers objectAtIndex:5] animated:YES];
+//	if(!_icyPresent) [self removeSpecifier:[_specifiers objectAtIndex:6] animated:YES];
+}
+
 - (id)specifiers {
 	return [self localizedSpecifiersForSpecifiers:[self loadSpecifiersFromPlistName:@"CyDelete" target:self]];
 }
@@ -9,5 +17,15 @@
 - (id)donationButton:(id)arg {
 	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4275311"]];
 }
+
+/*
+- (id)setCydiaProtection:(CFBooleanRef)value specifier:(id)specifier {
+	if(value == kCFBooleanFalse) {
+		if(!_icyPresent) value = kCFBooleanTrue;
+	}
+	[self setPreferenceValue:(id)value specifier:specifier];
+	[[NSUserDefaults standardUserDefaults] synchronize];
+}
+*/
 
 @end
